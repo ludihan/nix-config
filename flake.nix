9b2d6ef43a7e3b16db810a76cd356cf37d98a14f
ludihan <65617704+ludihan@users.noreply.github.com>
2025-12-08 12:11:15 -0300

    config cleanup

--- flake.nix
+++ flake.nix
@@ -21,42 +21,53 @@
     };
   };
 
-  outputs = inputs@{ self, nixpkgs, home-manager, nix-index-database, ... }:
-  let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      home-manager,
+      nix-index-database,
+      ...
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
 
-    hmBase = hostPath:
-      home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        extraSpecialArgs = { inherit inputs; };
-        modules = [
-          hostPath
-          nix-index-database.homeModules.nix-index
-          { programs.nix-index-database.comma.enable = true; }
-        ];
-      };
-  in
-  {
-    nixosConfigurations = {
-      nixos-desktop = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs; };
-        modules = [ ./hosts/desktop ];
+      hmBase =
+        hostPath:
+        home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          extraSpecialArgs = { inherit inputs; };
+          modules = [
+            hostPath
+            nix-index-database.homeModules.nix-index
+            { programs.nix-index-database.comma.enable = true; }
+          ];
+        };
+      noBase =
+        hostPath:
+        nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs; };
+          modules = [
+            hostPath
+          ];
+        };
+    in
+    {
+      nixosConfigurations = {
+        "nixos-desktop" = noBase ./hosts/desktop;
+        "nixos-laptop" = noBase ./hosts/laptop;
       };
 
-      nixos-laptop = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs; };
-        modules = [ ./hosts/laptop ];
+      homeConfigurations = {
+        "ludihan@nixos-desktop" = hmBase ./hosts/desktop/home.nix;
+        "ludihan@nixos-laptop" = hmBase ./hosts/laptop/home.nix;
       };
+
+      templates = inputs.templates;
+
+      packages.${system} = import ./pkgs { inherit inputs pkgs; };
+
+      formatter.${system} = pkgs.nixfmt-rfc-style;
     };
-
-    homeConfigurations = {
-      "ludihan@nixos-desktop" = hmBase ./hosts/desktop/home.nix;
-      "ludihan@nixos-laptop"  = hmBase ./hosts/laptop/home.nix;
-    };
-
-    templates = inputs.templates;
-
-    packages.${system} = import ./pkgs { inherit inputs pkgs; };
-  };
 }
